df0ee948bda37e6c007eaf1e7a503a655245802d
ezequiel-piki <ezequielpiki23@gmail.com>
2022-05-11 22:14:18 -0300

    finalizando seccion 10

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -36,4 +37,39 @@ borrarCliente(){
   return this.clientes.splice(0,1);
 }
 
+
+/* key value pipe */
+persona = {
+  nombre : 'Ezequiel',
+  edad : 28,
+  direccion : 'Berazategui,Argentina'
 }
+
+
+/* JsonPipe */
+
+heroes = [
+  {
+    nombre: 'Batman',
+    vuela:true
+  },
+  {
+    nombre: 'Capitan America',
+    vuela:false
+  },
+  {
+    nombre: 'Ironman',
+    vuela:true
+  }
+];
+/* Async pipe */
+miObservable = interval(5000) /* 0,1,2,3,4,5,6,7 */
+valorPromesa = new Promise ((resolve,reject)=>{
+
+  setTimeout(()=>{
+    resolve ('Tenemos data de promesa');
+  },3500)
+})
+
+}
+
